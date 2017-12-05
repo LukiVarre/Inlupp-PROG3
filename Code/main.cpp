b1344054840f1ef2dd2892a94b75c66016edbd8c
Peter Yakob <32325216+Peter-Yakob@users.noreply.github.com>
2017-12-05 14:58:37 +0100

    Revert "Nya filer tillagda"
    This reverts commit 5ba05d2f94bddb708d59335ee9a128d1c08447e7.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,13 +1,6 @@
-//
-// Created by lukas on 2017-12-05.
-//
 #include <iostream>
-#include "Window.h"
 
-
-using namespace gameEngine;
-
-int main(int argc, char** argv){
-
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
